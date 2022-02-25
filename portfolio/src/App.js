f7c61e88e7157bc11bc8d870b04696448b1a8555
Astrospence <illustrator.wood@gmail.com>
2022-02-24 20:51:51 -0700

    scaffolding components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import './App.css'
 
-function App() {
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+
+const App = () => {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <nav className='nav'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='home-main'>
+            <div className='home-main-banner'>
+                <h1>My Notable Projects</h1>
+            </div>
+            <div className='home-body'>
+                <div className='home-tile-1'>
+                    <h2>Project Here</h2>
+                </div>
+                <div className='home-tile-2'>
+                    <h2>Project Here</h2>
+                </div>
+                <div className='home-tile-3'>
+                    <h2>Project Here</h2>
+                </div>
+                <div className='home-tile-3'>
+                    <h2>Project Here</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home

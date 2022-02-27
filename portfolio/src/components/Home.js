08a488275c07487922bd368e5de223c26b41202b
Astrospence <illustrator.wood@gmail.com>
2022-02-27 13:50:10 -0700

    switched color profile

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -2,25 +2,22 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='home-main'>
-            <div className='home-main-banner'>
-                <h1>My Notable Projects</h1>
+        <div className='home-body'>
+            <h1>Projects</h1>
+            <div className='home-tile'>
+                <h2>Project Here</h2>
             </div>
-            <div className='home-body'>
-                <div className='home-tile'>
-                    <h2>Project Here</h2>
-                </div>
-                <div className='home-tile'>
-                    <h2>Project Here</h2>
-                </div>
-                <div className='home-tile'>
-                    <h2>Project Here</h2>
-                </div>
-                <div className='home-tile'>
-                    <h2>Project Here</h2>
-                </div>
+            <div className='home-tile'>
+                <h2>Project Here</h2>
+            </div>
+            <div className='home-tile'>
+                <h2>Project Here</h2>
+            </div>
+            <div className='home-tile'>
+                <h2>Project Here</h2>
             </div>
         </div>
+        
     )
 }
 

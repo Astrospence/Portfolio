3d7648eb716b5953613945b16c403475f12aed01
Astrospence <illustrator.wood@gmail.com>
2022-05-10 11:52:57 -0600

    renovating site and starting from scratch

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
-import './App.css'
-
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
+import './App.css' 
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <div className='banner'>
-          <h2>Spencer Wood's Portfolio</h2>
-          <nav className='nav'>
-            <Link to='/' className='links'>Home</Link>
-            <Link to='/about' className='links'>About</Link>
-            <Link to='/contact' className='links'>Contact</Link>
-          </nav>
-        </div>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 }

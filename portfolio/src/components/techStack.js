50e7bc106e233d7da60452b65608fa7885dabd0e
Astrospence <illustrator.wood@gmail.com>
2022-05-13 14:18:48 -0600

    creating tiles for tech stack

--- portfolio/src/components/techStack.js
+++ portfolio/src/components/techStack.js
@@ -3,7 +3,60 @@ import React from 'react'
 export default function TechStack() {
     return (
         <div className='techStack'>
-            <h2>My Current Tech Stack</h2>
+            <h2>My Growing Tech Stack</h2>
+            <div className='tileContainer'>
+                <div id='javascript' className='tile'>
+                    <h3>JavaScript</h3>
+                </div>
+                <div id='HTML' className='tile'>
+                    <h3>HTML</h3>
+                </div>
+                <div id='CSS' className='tile'>
+                    <h3>CSS</h3>
+                </div>
+                <div id='react' className='tile'>
+                    <h3>React.js</h3>
+                </div>
+                <div id='redux' className='tile'>
+                    <h3>Redux</h3>
+                </div>
+                <div id='axios' className='tile'>
+                    <h3>Axios</h3>
+                </div>
+                <div id='node' className='tile'>
+                    <h3>Node.js</h3>
+                </div>
+                <div id='express' className='tile'>
+                    <h3>Express</h3>
+                </div>
+                <div id='sql' className='tile'>
+                    <h3>SQL</h3>
+                </div>
+                <div id='sqlite' className='tile'>
+                    <h3>SQLite</h3>
+                </div>
+                <div id='postgresql' className='tile'>
+                    <h3>PostgreSQL</h3>
+                </div>
+                <div id='jest' className='tile'>
+                    <h3>Jest</h3>
+                </div>
+                <div id='cypress' className='tile'>
+                    <h3>Cypress</h3>
+                </div>
+                <div id='netlify' className='tile'>
+                    <h3>Netlify</h3>
+                </div>
+                <div id='heroku' className='tile'>
+                    <h3>Heroku</h3>
+                </div>
+                <div id='git' className='tile'>
+                    <h3>Git</h3>
+                </div>
+                <div id='vscode' className='tile'>
+                    <h3>VSCode</h3>
+                </div>
+            </div>
         </div>
     )
 }

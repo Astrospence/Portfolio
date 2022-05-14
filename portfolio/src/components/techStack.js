07bcb39ca21192fb90e291b9f4c4cb5ee7d524b9
Astrospence <illustrator.wood@gmail.com>
2022-05-14 07:57:58 -0600

    finished tech stack section

--- portfolio/src/components/techStack.js
+++ portfolio/src/components/techStack.js
@@ -20,36 +20,18 @@ export default function TechStack() {
                 <div id='redux' className='tile'>
                     <h3>Redux</h3>
                 </div>
-                <div id='axios' className='tile'>
-                    <h3>Axios</h3>
-                </div>
-                <div id='node' className='tile'>
-                    <h3>Node.js</h3>
-                </div>
-                <div id='express' className='tile'>
-                    <h3>Express</h3>
-                </div>
-                <div id='sql' className='tile'>
-                    <h3>SQL</h3>
-                </div>
-                <div id='sqlite' className='tile'>
-                    <h3>SQLite</h3>
-                </div>
-                <div id='postgresql' className='tile'>
-                    <h3>PostgreSQL</h3>
-                </div>
+                <div id='axios' className='tile'></div>
+                <div id='node' className='tile'></div>
+                <div id='express' className='tile'></div>
+                <div id='sql' className='tile'></div>
+                <div id='sqlite' className='tile'></div>
+                <div id='postgresql' className='tile'></div>
                 <div id='jest' className='tile'>
                     <h3>Jest</h3>
                 </div>
-                <div id='cypress' className='tile'>
-                    <h3>Cypress</h3>
-                </div>
-                <div id='netlify' className='tile'>
-                    <h3>Netlify</h3>
-                </div>
-                <div id='heroku' className='tile'>
-                    <h3>Heroku</h3>
-                </div>
+                <div id='cypress' className='tile'></div>
+                <div id='netlify' className='tile'></div>
+                <div id='heroku' className='tile'></div>
                 <div id='git' className='tile'>
                     <h3>Git</h3>
                 </div>

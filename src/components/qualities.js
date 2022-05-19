adb7124535339567ef020fd6cbd6d7bd4aa263ce
Astrospence <illustrator.wood@gmail.com>
2022-05-19 12:14:24 -0600

    adjusted styles for breakpoints

--- src/components/qualities.js
+++ src/components/qualities.js
@@ -6,12 +6,12 @@ export default function Qualities() {
             <div className='qualities'>
                 <h2>Why You Should Hire Me</h2>
                 <ul>
-                    <li>- I bring skill to the table, with a guarantee that I will continuously improve my skill.</li>
+                    <li>- I bring skill to the table, with motivation to continuously improve my skill.</li>
                     <li>- I'm teachable and eager to learn from mentors.</li>
                     <li>- I'm loyal and will apply my best efforts to delivering on my responsibilities.</li>
                     <li>- I take personal pride in a job well done.</li>
                     <li>- My biggest drive is to improve myself and be a valuable team member.</li>
-                    <li>- I can connect with most anyone and I'm a pleasant personality to add to any team.</li>
+                    <li>- I get along with people and I'm a pleasant personality to add to any team.</li>
                 </ul>
             </div>
             <img src='https://images.unsplash.com/photo-1475506631979-72412c606f4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='Foosball table closeup'/>
